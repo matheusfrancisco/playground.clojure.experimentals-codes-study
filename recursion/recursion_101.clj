3c35d79c19169f2a70ca1e9896ae034b61627641
matheusfrancisco <matheusmachadoufsc@gmail.com>
2021-02-23 22:55:01 -0300

    learn: about recur

--- recursion/recursion_101.clj
+++ recursion/recursion_101.clj
@@ -37,5 +37,55 @@
 (map* str (range 3)) ;=> ("0" "1" "2")
 
 
+;; non-tail recursion
+(defn filter* [p? ls]
+  (if (empty? ls)
+    ()
+    (if (p? (first ls))
+      (cons (first ls) (filter* p? (rest ls)))
+      (filter* p? (rest ls)))))
+
+(defn filter*-helper [p? ls acc]
+  (if (empty? ls)
+    acc
+    (if (p? (first ls))
+      (recur p? (rest ls) (conj acc (first ls)))
+      (recur p? (rest ls) acc))))
+
+;; tail recursion
+(defn filter* [p? ls]
+  (filter*-helper p? ls []))
+
+(filter* even? (range 45))
+(filter* nil? (range 45))
+(filter* (complement nil?) (range 45))
+
+(defn map*-helper [f ls acc]
+  (if (empty? ls)
+    acc
+    (recur f (rest ls) (conj acc (f (first ls))))))
+
+(defn map*-tail [f ls]
+  (map*-helper f ls []))
+
+
+(map*-tail str (range 3))
+
+(defn length-helper [ls acc]
+  (if (empty? ls)
+    acc
+    (recur (rest ls) (+ 1 acc))))
+
+(defn length [ls]
+  (length-helper ls 0))
+
+
+#_(defn read-book [book]
+  (if (empty? book)
+    nil
+    (do
+      (read (first-page book))
+      (recur (rest-of book)))))
+
 (comment
   (run-tests *ns*))
