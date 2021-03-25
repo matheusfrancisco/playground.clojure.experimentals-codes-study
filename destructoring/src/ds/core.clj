4017ceed05aac0d401dee2cb34d01703a1664af3
matheusfrancisco <matheusmachadoufsc@gmail.com>
2021-03-24 21:27:10 -0300

    learning more about clj

--- destructoring/src/ds/core.clj
+++ destructoring/src/ds/core.clj
@@ -27,3 +27,43 @@
 ; conjure/out | (2 3 4 5)
 
 ;;video-3
+
+(defn f-map [d {:keys [b a c] :as m} & rst]
+  (prn m)
+  (prn d)
+  (prn b c a)
+  (prn rst))
+
+(f-map 1 {:a 2 :b 3 :c 4} 3 4 5)
+
+
+(defn print-formatted-name [person]
+  (let [first-name (:first-name person)
+        last-name (:last-name person)]
+    (println (str first-name " " last-name))))
+
+(defn print-formatted-name [{:keys [first-name last-name]
+                             :or {last-name "Xicao"}}]
+  (println (str first-name " " last-name)))
+
+(print-formatted-name {:first-name "Matheus" :last-name "Machado"})
+(print-formatted-name {:first-name "Matheus" })
+
+
+
+(defn f-map [a {a2 :a
+                :keys [b c d]
+                :strs [l]
+                :syms [s]
+                :or {d 10}
+                :as _m} & rst]
+  (prn a)
+  (prn a2)
+  (prn b)
+  (prn c)
+  (prn d)
+  (prn l, s)
+  (prn rst))
+
+(f-map 1 {:a 2 :b 3 :c 4 "l" 10 's 3} 3 4 5)
+
