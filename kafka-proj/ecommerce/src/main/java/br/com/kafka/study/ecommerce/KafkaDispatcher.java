37f691f1e58fae3fd0a0ad59835451ce86ab5a29
matheusfrancisco <matheusmachadoufsc@gmail.com>
2021-04-19 15:06:28 -0300

    +learn about kafka consumer and producer

---
+++ kafka-proj/ecommerce/src/main/java/br/com/kafka/study/ecommerce/KafkaDispatcher.java
@@ -0,0 +1,45 @@
+package br.com.kafka.study.ecommerce;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.Closeable;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+class KafkaDispatcher implements Closeable {
+    private final KafkaProducer<String, String> producer;
+    KafkaDispatcher() {
+        this.producer =  new KafkaProducer<String, String>(properties());
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+
+    public void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
+        var record = new ProducerRecord<String, String>(topic, value, value);
+
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("sucesso enviando" + data.topic() + "::partition" + data.partition() + "/ offse" + data.offset());
+        };
+
+        producer.send(record, callback).get();
+    }
+
+    @Override
+    public void close() {
+        producer.close();
+    }
+}
--- kafka-proj/ecommerce/src/main/java/br/com/kafka/study/ecommerce/KafkaService.java
+++ kafka-proj/ecommerce/src/main/java/br/com/kafka/study/ecommerce/KafkaService.java
@@ -4,18 +4,20 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
 
-public class KafkaService {
+public class KafkaService implements Closeable {
     private final KafkaConsumer<String, String> consumer;
     private final ConsumerFunction parse;
 
-    public KafkaService(String topic, ConsumerFunction parse) {
+    public KafkaService(String groupId, String topic, ConsumerFunction parse) {
         this.parse = parse;
-        this.consumer = new KafkaConsumer<String, String>(properties());
+        this.consumer = new KafkaConsumer<String, String>(properties(groupId));
         consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
     }
 
@@ -31,15 +33,19 @@ public class KafkaService {
         }
     }
 
-    private static Properties properties() {
+    private static Properties properties(String groupId) {
         var properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "-" + UUID.randomUUID().toString());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
         properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
         return properties;
     }
 
+    @Override
+    public void close()  {
+        consumer.close();
+    }
 }
